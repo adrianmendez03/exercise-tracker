6028333f075da20946f3936b83b4329fb6be212a
Adrian Mendez <adrianmndz328@gmail.com>
2020-07-18 23:20:02 -0500

    2

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use('/', express.static(path.join(__dirname, '../client/build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, '..client/build', 'index.html'));
     })
 }
 
